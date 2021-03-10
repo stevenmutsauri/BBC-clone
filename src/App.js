60d57caaa1bd7f1953c15d594097f81778d326d5
akashvaghela09 <akashvaghela09@gmail.com>
2021-03-10 15:06:26 +0530

    commoncard added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Footer from './Component/Footer/Footer';
+import { Home } from './Routes/Home';
 
 function App() {
   return (
     <div className="App">
      <Navbar></Navbar>
+     <Home/>
      <Footer></Footer>
     </div>
   );
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,1 +1,14 @@
-//test
+import React from 'react';
+import { CommonCard } from '../Component/Home/CommonCard';
+
+const Home = () => {
+
+    
+    return (
+        <div>
+            <CommonCard />
+        </div>
+    )
+}
+
+export {Home}
