894246d02094fc17e494c95892c6a982b5ffff66
Abhi <abhi@gmail.com>
2021-03-10 15:57:03 +0530

    modified actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Component/Navbar/Navbar';
 import Footer from './Component/Footer/Footer';
+import Welcome from './Component/Home/Welcome';
 
 function App() {
   return (
     <div className="App">
      <Navbar></Navbar>
+     
      <Footer></Footer>
     </div>
   );
