ef7e8e4703ef11db4940349ea2f52146a6da6bcb
akashvaghela09 <akashvaghela09@gmail.com>
2021-03-10 15:42:51 +0530

    Redux added

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { CommonCard } from '../Component/Home/CommonCard';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { CommonCard } from '../Component/Home/CommonCard';
+import {fetchNewsData} from "../Redux/app/action"
 
 const Home = () => {
 
-    
+  const dispatch = useDispatch()
+  const data = useSelector(state => state.app.data)
+  console.log(data)
+
+  useEffect(()=> {
+      dispatch(fetchNewsData())
+  }, [dispatch])
+
     return (
         <div>
-            <CommonCard />
+            {/* <CommonCard /> */}
         </div>
     )
 }
