8fe2633ba30a62ad0935eade33907348c2302046
akashvaghela09 <akashvaghela09@gmail.com>
2021-03-16 14:03:03 +0530

    homepage styling changed

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -8,13 +8,9 @@ import { WeatherForecast } from "../Component/Home/WeatherForecast";
 import Welcome from "../Component/Home/Welcome";
 import { BannerAd} from '../Component/Advertisement/BannerAd';
 import { PopupAd } from '../Component/Advertisement/PopupAd';
-import { useDispatch } from "react-redux";
-import { fetchNewsData } from "../Redux/app/action";
+
 const Home = () => {
-  const dispatch = useDispatch()
-  React.useEffect(() => {
-    dispatch(fetchNewsData())
-  },[dispatch])
+
   return (
     <div>
       <BannerAd />
